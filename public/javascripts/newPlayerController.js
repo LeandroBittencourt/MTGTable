28e55f7ccab506bfd7032c2206df2428a4bbf006
Leandro Bittencourt <leandro.dias.bittencourt@gmail.com>
2015-09-26 03:17:23 -0300

    Adicionado Angular ao projeto
    Angular com ação de submit no botão, próximo passo incluir o mongoDB

---
+++ public/javascripts/newPlayerController.js
@@ -0,0 +1,9 @@
+var app = angular.module('newPlayerController',[]);
+
+app.controller('submitNewPlayerController', function ($scope){
+  $scope.formModel = {};
+
+  $scope.onSubmit = function ({
+    
+  });
+});
--- routes/newPlayer.js
+++ routes/newPlayer.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('newPlayer');
+  res.render('newPlayer',{controller:'newPlayerController'});
 });
 
 module.exports = router;
